62ae4aaf34a42f7a5d0b2be7c35da47d93754567
Enmanuel Almonte <ealmonterozon@gmail.com>
2023-11-07 11:47:15 -0400

    add operations lambda

---
+++ operations/app.mjs
@@ -0,0 +1,36 @@
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA,
+});
+export const lambdaHandler = async (event, context) => {
+  try {
+    const promise = new Promise((resolve, reject) => {
+      connection.query("SELECT * FROM operations", (err, result, values) => {
+        if (!err) {
+          connection.end();
+          resolve(result);
+        } else {
+          console.log("Error executing query: " + err.message);
+          reject(err);
+        }
+      });
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        data: await promise,
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
